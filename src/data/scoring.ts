465b9b3a5a368b84b060e7de011be28c62bf64d1
Luke Inglis <lukeinglis21@yahoo.com>
2026-07-02 10:14:10 -0400

    Update hardcoded knockout results
    Signed-off-by: Luke Inglis <lukeinglis21@yahoo.com>

--- src/data/scoring.ts
+++ src/data/scoring.ts
@@ -84,7 +84,9 @@ const HARDCODED_KNOCKOUT_RESULTS: Record<string, string> = {
   "round_of_32_5": "NOR",  // CIV 1:2 NOR (Jun 30)
   "round_of_32_6": "FRA",  // FRA 3:0 SWE (Jun 30)
   "round_of_32_7": "MEX",  // MEX 2:0 ECU (Jul 1)
+  "round_of_32_8": "USA",  // USA 2:0 BIH (Jul 2)
   "round_of_32_9": "ENG",  // ENG 2:1 COD (Jul 1)
+  "round_of_32_10": "BEL",  // BEL 3:2 SEN (Jul 1)
 };
 
 // Active knockout results: starts from hardcoded, overwritten by live API when available
